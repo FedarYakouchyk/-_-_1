7b14c43f3b07bd5e2ee29cd87c976ace193096b7
Fedor Yakouchyk <fedar77@outlook.com>
2023-01-19 02:13:20 +0300

    Все 4 таска готовы к загрузке

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите любое целое число");
+int Num = Convert.ToInt32(Console.ReadLine());
+int Ost = Num%2;
+if (Ost==0)
+{
+  Console.WriteLine($"Число:{Num} является чётным");  
+}
+else
+{
+   Console.WriteLine($"Число:{Num} является нечётным"); 
+}
